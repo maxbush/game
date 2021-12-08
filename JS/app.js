be2de8cd095965cdeaf80852399c238ed6cf4959
maxbush <maxim.bushenko@gmail.com>
2021-12-09 00:53:41 +0300

    first commit

---
+++ JS/app.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", () => {
+	const grid = document.querySelector(".grid");
+	const width = 8;
+	const squares = [];
+
+	// Create board
+	function createBoard() {
+		for (let i = 0; i < width * width; i++) {
+			const square = document.createElement("div");
+			grid.appendChild(square);
+			squares.push(square);
+		}
+	}
+
+	createBoard();
+});
