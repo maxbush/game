bd1570092bf4b8ee0373da2fa39ef0ccedd4d5af
maxbush <maxim.bushenko@gmail.com>
2021-12-09 01:37:24 +0300

    added random squart colors

--- JS/app.js
+++ JS/app.js
@@ -3,10 +3,14 @@ document.addEventListener("DOMContentLoaded", () => {
 	const width = 8;
 	const squares = [];
 
+	const ballsColors = ["red", "yellow", "orange", "purple", "green", "blue"];
+
 	// Create board
 	function createBoard() {
 		for (let i = 0; i < width * width; i++) {
 			const square = document.createElement("div");
+			let randomColor = Math.floor(Math.random() * ballsColors.length);
+			square.style.backgroundColor = ballsColors[randomColor];
 			grid.appendChild(square);
 			squares.push(square);
 		}
