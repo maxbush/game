1fc3835c5fc91a4f0bdebaf06f40f054355b8dc3
maxbush <maxim.bushenko@gmail.com>
2021-12-09 21:32:49 +0300

    added comments

--- JS/app.js
+++ JS/app.js
@@ -7,12 +7,18 @@ document.addEventListener("DOMContentLoaded", () => {
 
 	// Create board
 	function createBoard() {
-		for (let i = 0; i < width * width; i++) {
-			const square = document.createElement("div");
-			let randomColor = Math.floor(Math.random() * ballsColors.length);
+		for (
+			let i = 0;
+			i < width * width;
+			i++ //нижеследующая конструкция повторится width*width раз (64)
+		) {
+			const square = document.createElement("div"); //создаем div
+			square.setAttribute("draggable", true);
+			square.setAttribute("id", i); //каждый круг i имеет новое значение 0-63 которое становится id
+			let randomColor = Math.floor(Math.random() * ballsColors.length); //Math.random от нуля до длины массива (пяти) (массив из 6), округленный в меньшую сторону
 			square.style.backgroundColor = ballsColors[randomColor];
-			grid.appendChild(square);
-			squares.push(square);
+			grid.appendChild(square); //пихаем див в переменную grid которая возвращает  .grid
+			squares.push(square); // складываем в массив
 		}
 	}
 
